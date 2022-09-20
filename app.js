c11c76233395971cfabc11b9b16bcbc9f9152af8
patofernandez <64528852+patriciodfernandez@users.noreply.github.com>
2022-09-20 10:22:42 -0300

    asd

--- app.js
+++ app.js
@@ -6,8 +6,40 @@ const app = new App ({
     port: process.envPORT || 3000
 });
 
-
-(async()=>{
-    await app.start();
-     
-})
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say({
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Hey there <@${message.user}>!`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Click Me"
+            },
+            "action_id": "button_click"
+          }
+        }
+      ],
+      text: `Hey there <@${message.user}>!`
+    });
+  });
+  
+  app.action('button_click', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`);
+  });
+  
+  (async () => {
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+  
+    console.log('⚡️ Bolt app is running!');
+  })();
