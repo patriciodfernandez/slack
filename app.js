d515ba61146bbffb299409a3a43691b889403e64
patofernandez <64528852+patriciodfernandez@users.noreply.github.com>
2022-09-20 08:48:42 -0300

    started heroku

---
+++ app.js
@@ -0,0 +1,13 @@
+const {App} = require('@slack/bolt')
+
+const app = new App ({
+    token: 'xoxb-4077033331937-4105870885442-GwQK1JlfvDnhGbd9Gbywetm4',
+    signingSecret: '219e55d8d24c3b4b05ca82b6ed96d051',
+    port: process.envPORT || 3000
+});
+
+
+(async()=>{
+    await app.start();
+     
+})
