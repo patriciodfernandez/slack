e01255f9c16a4175675f6e7486a9f0e167b9a499
patofernandez <64528852+patriciodfernandez@users.noreply.github.com>
2022-09-20 09:54:21 -0300

    as

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const {App} = require('@slack/bolt')
 
 const app = new App ({
-    token: 'xoxb-4077033331937-4105870885442-GwQK1JlfvDnhGbd9Gbywetm4',
+    token: 'xoxb-4077033331937-4105870885442-BFnhNhLvbzXWexpqClev0fMs',
     signingSecret: '219e55d8d24c3b4b05ca82b6ed96d051',
     port: process.envPORT || 3000
 });
